71427007d87eb29138ca8eb54b0b7f829b9096e9
Alexander Ho <alexhsz98@gmail.com>
2022-07-09 18:32:35 +0100

    Adding PSID logging

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.post('/webhook', (req, res) => {
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+
+        // Get the sender PSID 
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
       });
   
       // Returns a '200 OK' response to all requests
@@ -62,3 +66,18 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+  // Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
